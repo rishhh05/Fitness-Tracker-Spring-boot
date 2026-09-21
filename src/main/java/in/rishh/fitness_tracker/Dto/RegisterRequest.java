2d74bffab14d4132277beac1ac82783e3d76c2ab
Rishabh S <shivharerishabh86@gmail.com>
2026-09-22 01:23:50 +0530

    Validations

--- src/main/java/in/rishh/fitness_tracker/Dto/RegisterRequest.java
+++ src/main/java/in/rishh/fitness_tracker/Dto/RegisterRequest.java
@@ -1,5 +1,7 @@
 package in.rishh.fitness_tracker.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Invalid Email")
     private String email;
+
+    @NotBlank(message = "Password is mandatory")
     private String Password;
     private String firstName;
     private String lastName;
